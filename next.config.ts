1d9899c6465920e3a16a767c9b40d2c9e1ba0ef4
Riki Kusnadi <rikiamir.kusnadi@gmail.com>
2025-08-05 14:58:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
